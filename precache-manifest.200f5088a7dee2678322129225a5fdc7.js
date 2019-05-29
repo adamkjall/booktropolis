a82465043ba60fb4eceac3c94f58c3f369b2828c
Adam <bottarn.botvidsson@gmail.com>
2019-05-29 19:40:49 +0200

    Updates

---
+++ precache-manifest.200f5088a7dee2678322129225a5fdc7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b1e22190c043e030f835e980f5df185",
+    "url": "/adamkjall/booktropolis/index.html"
+  },
+  {
+    "revision": "eb1eb8ddcb7c944b4cd2",
+    "url": "/adamkjall/booktropolis/static/css/2.f6ec77e9.chunk.css"
+  },
+  {
+    "revision": "62c6373f0c4bb8161236",
+    "url": "/adamkjall/booktropolis/static/css/main.121ae799.chunk.css"
+  },
+  {
+    "revision": "eb1eb8ddcb7c944b4cd2",
+    "url": "/adamkjall/booktropolis/static/js/2.fcd1372f.chunk.js"
+  },
+  {
+    "revision": "62c6373f0c4bb8161236",
+    "url": "/adamkjall/booktropolis/static/js/main.37bdb694.chunk.js"
+  },
+  {
+    "revision": "8fb7b2a6b60d56085d9d",
+    "url": "/adamkjall/booktropolis/static/js/runtime~main.38344f68.js"
+  },
+  {
+    "revision": "387e7e3577602eadb9c60760301ad96b",
+    "url": "/adamkjall/booktropolis/static/media/leafy.387e7e35.woff"
+  }
+]);
